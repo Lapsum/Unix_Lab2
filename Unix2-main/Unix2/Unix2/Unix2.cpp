f62651babf7a603b0185490dd97e8359af40e3d7
Lapsum <76732987+Lapsum@users.noreply.github.com>
2022-04-11 02:43:12 +0700

    Add files via upload

---
+++ Unix2-main/Unix2/Unix2/Unix2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <thread>	
+#include <mutex>	
+#include <condition_variable>	
+#include <chrono>
+
+using namespace std;
+
+condition_variable cv;
+mutex cv_m;
+
+bool isProvided = false;
+int counter = 0;
+
+void provide()
+{
+	while (true) {
+		unique_lock<mutex> lk(cv_m);
+		counter++;
+		cout << "provide: " << counter << endl;
+		isProvided = true;
+		cv.notify_all();
+		lk.unlock();		
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+}
+void consume()
+{
+	while (true) {
+		unique_lock<mutex> lk(cv_m);
+		cv.wait(lk, [] {return isProvided; });
+		cout << "consume: " << counter << endl;
+		isProvided = false;
+		lk.unlock();
+	}
+}
+
+int main()
+{
+	thread thread1(provide);
+	thread thread2(consume);
+
+	thread1.join();
+	thread2.join();
+
+	return 0;
+}
